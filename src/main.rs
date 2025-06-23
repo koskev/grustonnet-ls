7abc49a71b3fb80a9552571c9f0b02d97d848f1d
Kevin <kevin@kokev.de>
2025-06-23 18:13:42 +0200

    Restructure main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,4 @@
-use std::error::Error;
-
-use crate::server::{JsonnetServer, LSPResponse, LSPServer};
-use anyhow::Result;
-use lsp_server::{
-    Connection, ErrorCode, ExtractError, Message, Notification, Request, RequestId, Response,
-    ResponseError,
-};
-use lsp_types::{
-    InitializeParams,
-    notification::{DidChangeConfiguration, DidChangeTextDocument, DidOpenTextDocument},
-    request::Completion,
-};
+use crate::server::{JsonnetServer, LSPServerManager};
 
 mod bridge;
 mod cache;
@@ -27,124 +15,9 @@ pub mod binding {
 
 #[tokio::main]
 async fn main() {
-    let server = JsonnetServer::new();
-    main_loop(server).unwrap()
-}
-
-fn main_loop<S: LSPServer>(server: S) -> Result<(), Box<dyn Error + Sync + Send>> {
-    //let (connection, io_threads) = Connection::stdio();
-    let (connection, io_threads) = Connection::listen("127.0.0.1:4874").unwrap();
-
-    let server_capabilities = serde_json::to_value(server.get_capabilities()).unwrap();
-    let params = connection
-        .initialize(server_capabilities)
-        .expect("init connection");
-
-    let _params: InitializeParams = serde_json::from_value(params).unwrap();
-    eprintln!("starting example main loop");
-    for msg in &connection.receiver {
-        eprintln!("got msg: {msg:?}");
-        match msg {
-            Message::Request(req) => {
-                if connection.handle_shutdown(&req)? {
-                    return Ok(());
-                }
-                eprintln!("got request: {req:?}");
-                let resp = handle_request(req.clone(), &server);
-                let result: Result<serde_json::Value, ResponseError> = match resp {
-                    Ok(val) => Ok(val.into()),
-                    Err(e) => Err(e),
-                };
-
-                eprintln!("Sending response {:?}", result);
-
-                connection.sender.send(Message::Response(Response {
-                    id: req.id,
-                    result: result.clone().ok(),
-                    error: result.err(),
-                }))?
-            }
-            Message::Response(resp) => {
-                eprintln!("got response: {resp:?}");
-            }
-            Message::Notification(not) => {
-                let _ = handle_notification(not.clone(), &server);
-            }
-        }
-    }
-    io_threads.join().unwrap();
-    Ok(())
-}
-
-macro_rules! lsp_handle_notification {
-    ($server: expr, $name:ident, $param:ty, $req: expr) => {
-        match cast_notification::<$param>($req) {
-            Ok(params) => {
-                match $server.$name(params) {
-                    Ok(_) => (),
-                    Err(e) => eprintln!("Notification failed: {}", e),
-                };
-                return Ok(());
-            }
-            Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
-            Err(ExtractError::MethodMismatch(req)) => req,
-        }
+    let server = LSPServerManager {
+        server: JsonnetServer::new(),
     };
-}
-
-fn handle_notification<S: LSPServer>(req: Notification, server: &S) -> Result<(), ResponseError> {
-    let mut req = lsp_handle_notification!(
-        server,
-        did_change_configuration,
-        DidChangeConfiguration,
-        req
-    );
-    req = lsp_handle_notification!(server, did_change_text, DidChangeTextDocument, req);
-    req = lsp_handle_notification!(server, did_open, DidOpenTextDocument, req);
-
-    let _ = req;
-    Err(ResponseError {
-        code: ErrorCode::MethodNotFound as i32,
-        message: "Method not implemented".into(),
-        data: None,
-    })
-}
-
-macro_rules! lsp_handle_request {
-    ($server: expr, $name:ident, $param:ty, $req: expr) => {
-        match cast_req::<$param>($req) {
-            Ok((_id, params)) => {
-                let resp = $server.$name(params);
-                return resp;
-            }
-            Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
-            Err(ExtractError::MethodMismatch(req)) => req,
-        }
-    };
-}
-
-fn handle_request<S: LSPServer>(req: Request, server: &S) -> Result<LSPResponse, ResponseError> {
-    let _req = lsp_handle_request!(server, completion, Completion, req);
-
-    Err(ResponseError {
-        code: ErrorCode::MethodNotFound as i32,
-        message: "Method not implemented".into(),
-        data: None,
-    })
-}
-
-fn cast_req<R>(req: Request) -> Result<(RequestId, R::Params), ExtractError<Request>>
-where
-    R: lsp_types::request::Request,
-    R::Params: serde::de::DeserializeOwned,
-{
-    req.extract(R::METHOD)
-}
-
-fn cast_notification<R>(not: Notification) -> Result<R::Params, ExtractError<Notification>>
-where
-    R: lsp_types::notification::Notification,
-    R::Params: serde::de::DeserializeOwned,
-{
-    not.extract(R::METHOD)
+    server.run().unwrap();
+    //main_loop(server).unwrap()
 }
--- src/server.rs
+++ src/server.rs
@@ -1,12 +1,15 @@
 use anyhow::{Result, anyhow};
-use lsp_server::{ErrorCode, ResponseError};
+use lsp_server::{
+    Connection, ErrorCode, ExtractError, Message, Notification, Request, RequestId, Response,
+    ResponseError,
+};
 use lsp_types::{
-    CompletionItem, CompletionItemKind, CompletionList, CompletionOptions, CompletionParams,
-    CompletionResponse, DidChangeConfigurationParams, DidChangeTextDocumentParams,
-    DidOpenTextDocumentParams, DocumentDiagnosticParams, DocumentDiagnosticReportResult,
-    GotoDefinitionParams, InitializeParams, PublishDiagnosticsParams,
+    CompletionList, CompletionOptions, CompletionParams, CompletionResponse,
+    DidChangeConfigurationParams, DidChangeTextDocumentParams, DidOpenTextDocumentParams,
+    DocumentDiagnosticParams, DocumentDiagnosticReportResult, InitializeParams,
     RelatedFullDocumentDiagnosticReport, ServerCapabilities, TextDocumentSyncKind,
-    TextDocumentSyncOptions, notification::DidOpenTextDocument, request::DocumentDiagnosticRequest,
+    TextDocumentSyncOptions,
+    notification::{DidChangeConfiguration, DidChangeTextDocument, DidOpenTextDocument},
 };
 use ropey::Rope;
 use serde::Serialize;
@@ -15,7 +18,6 @@ use crate::{
     cache::Cache,
     completion::{Completion, global::GlobalCompletion, keyword::KeywordCompletion},
     diagnostics::{Diagnostics, eval::EvalDiagnostics},
-    node::{NodeKind, TypedDebug},
 };
 
 macro_rules! lsp_function_req {
@@ -30,6 +32,35 @@ macro_rules! lsp_function_not {
     ($name:ident, $param:ty) => {
         fn $name(&self, params: $param) -> Result<()> {
             Err(anyhow!("Not implemented"))
+        }
+    };
+}
+
+macro_rules! lsp_handle_request {
+    ($server: expr, $name:ident, $param:ty, $req: expr) => {
+        match cast_req::<$param>($req) {
+            Ok((_id, params)) => {
+                let resp = $server.$name(params);
+                return resp;
+            }
+            Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
+            Err(ExtractError::MethodMismatch(req)) => req,
+        }
+    };
+}
+
+macro_rules! lsp_handle_notification {
+    ($server: expr, $name:ident, $param:ty, $req: expr) => {
+        match cast_notification::<$param>($req) {
+            Ok(params) => {
+                match $server.$name(params) {
+                    Ok(_) => (),
+                    Err(e) => eprintln!("Notification failed: {}", e),
+                };
+                return Ok(());
+            }
+            Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
+            Err(ExtractError::MethodMismatch(req)) => req,
         }
     };
 }
@@ -58,6 +89,100 @@ fn not_implemented_error() -> ResponseError {
         message: "Method not implemented".into(),
         data: None,
     }
+}
+
+pub struct LSPServerManager<S: LSPServer> {
+    pub server: S,
+}
+
+impl<S: LSPServer> LSPServerManager<S> {
+    pub fn run(&self) -> Result<()> {
+        let (connection, io_threads) = Connection::listen("127.0.0.1:4874").unwrap();
+
+        let server_capabilities = serde_json::to_value(self.server.get_capabilities()).unwrap();
+        let params = connection
+            .initialize(server_capabilities)
+            .expect("init connection");
+
+        let _params: InitializeParams = serde_json::from_value(params).unwrap();
+        eprintln!("starting example main loop");
+        for msg in &connection.receiver {
+            eprintln!("got msg: {msg:?}");
+            match msg {
+                Message::Request(req) => {
+                    if connection.handle_shutdown(&req)? {
+                        return Ok(());
+                    }
+                    eprintln!("got request: {req:?}");
+                    let resp = self.handle_request(req.clone());
+                    let result: Result<serde_json::Value, ResponseError> = match resp {
+                        Ok(val) => Ok(val.into()),
+                        Err(e) => Err(e),
+                    };
+
+                    eprintln!("Sending response {:?}", result);
+
+                    connection.sender.send(Message::Response(Response {
+                        id: req.id,
+                        result: result.clone().ok(),
+                        error: result.err(),
+                    }))?
+                }
+                Message::Response(resp) => {
+                    eprintln!("got response: {resp:?}");
+                }
+                Message::Notification(not) => {
+                    let _ = self.handle_notification(not.clone());
+                }
+            }
+        }
+        io_threads.join().unwrap();
+        Ok(())
+    }
+    fn handle_request(&self, req: Request) -> Result<LSPResponse, ResponseError> {
+        let _req =
+            lsp_handle_request!(self.server, completion, lsp_types::request::Completion, req);
+
+        Err(ResponseError {
+            code: ErrorCode::MethodNotFound as i32,
+            message: "Method not implemented".into(),
+            data: None,
+        })
+    }
+
+    fn handle_notification(&self, req: Notification) -> Result<(), ResponseError> {
+        let mut req = lsp_handle_notification!(
+            self.server,
+            did_change_configuration,
+            DidChangeConfiguration,
+            req
+        );
+        req = lsp_handle_notification!(self.server, did_change_text, DidChangeTextDocument, req);
+        req = lsp_handle_notification!(self.server, did_open, DidOpenTextDocument, req);
+
+        let _ = req;
+        Err(ResponseError {
+            code: ErrorCode::MethodNotFound as i32,
+            message: "Method not implemented".into(),
+            data: None,
+        })
+    }
+}
+
+fn cast_req<R>(req: Request) -> Result<(RequestId, R::Params), ExtractError<Request>>
+where
+    R: lsp_types::request::Request,
+    R::Params: serde::de::DeserializeOwned,
+{
+    req.extract(R::METHOD)
+}
+
+fn cast_notification<R>(not: Notification) -> Result<R::Params, ExtractError<Notification>>
+where
+    R: lsp_types::notification::Notification,
+    R::Params: serde::de::DeserializeOwned,
+{
+    not.extract(R::METHOD)
 }
 
 // TODO: Do Generic magic?
