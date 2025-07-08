861d8e4d8cce1e96542291eccded0d55a8d49f0a
Kevin <kevin@kokev.de>
2025-07-08 20:27:26 +0200

    Add test case for multiple binaries

--- crates/grustonnet-ls-lib/tests/completion/local.rs
+++ crates/grustonnet-ls-lib/tests/completion/local.rs
@@ -335,6 +335,38 @@ fn binary_override_value() {
 }
 
 #[test]
+fn binary_multiple() {
+    CompletionTestCase {
+        filename: "testdata/complete/binary/multiple.jsonnet".into(),
+        replace_string: "x: a".into(),
+        replace_by_string: "x: a.".into(),
+        expected: CompletionList {
+            is_incomplete: false,
+            items: vec![
+                CompletionItem {
+                    label: "four".to_string(),
+                    ..Default::default()
+                },
+                CompletionItem {
+                    label: "three".to_string(),
+                    ..Default::default()
+                },
+                CompletionItem {
+                    label: "two".to_string(),
+                    ..Default::default()
+                },
+                CompletionItem {
+                    label: "one".to_string(),
+                    ..Default::default()
+                },
+            ],
+        },
+        config: local_config(),
+    }
+    .check();
+}
+
+#[test]
 fn self_simple() {
     CompletionTestCase {
         filename: "testdata/complete/self/simple.jsonnet".into(),
