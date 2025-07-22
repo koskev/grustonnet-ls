7aeb603156bacacdec744d4d6e6a46afb8acc14d
Kevin <kevin@kokev.de>
2025-07-22 22:03:30 +0200

    Add workaround for goto of local functions

--- crates/grustonnet-ls-lib/src/definition/mod.rs
+++ crates/grustonnet-ls-lib/src/definition/mod.rs
@@ -30,6 +30,36 @@ impl<'a> DefinitionProvider<'a> {
 
         let mut document_stack = doc.get_ast()?.get_stack_by_position(&(pos.clone()));
 
+        // Special case: If we goto the name of a local function
+        // TODO: what about a function definition itself?
+        if let NodeKind::Function(_func) = document_stack
+            .peek()
+            .ok_or(anyhow!("Empty document stack"))?
+            .node_kind
+            .as_ref()
+        {
+            // TODO: make a ast fix function and fix the local bind position
+            // If we have a local with a function the bind won't have a valid location
+            // Unwrap is safe
+            let func_node = document_stack.stack.pop().unwrap();
+            let local_node = document_stack.stack.pop().ok_or(anyhow!("no next local"))?;
+            let NodeKind::Local(local) = local_node.node_kind.as_ref() else {
+                return Err(anyhow!("next node is not local"));
+            };
+            let mut end_pos = func_node.node_base.loc_range.begin.clone();
+            end_pos.column += local_node.get_name().len() as i32;
+            return Ok(DefinitinInfo {
+                name: local.get_name().unwrap_or_default(),
+                location: lsp_types::Location {
+                    uri: uri.clone(),
+                    range: Range {
+                        start: func_node.node_base.loc_range.begin.clone().into(),
+                        end: func_node.node_base.loc_range.begin.clone().into(),
+                    },
+                },
+            });
+        }
+
         let (last_node, built_node) = document_stack.build_except_last(self.cache)?;
 
         let index_name = last_node
