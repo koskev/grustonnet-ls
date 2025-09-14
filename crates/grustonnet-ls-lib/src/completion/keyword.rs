6fa68792ab9cacd3d08b34a7d30220de60265eae
Kevin <kevin@kokev.de>
2025-09-14 16:01:09 +0200

    Refactor and update keyword list

--- crates/grustonnet-ls-lib/src/completion/keyword.rs
+++ crates/grustonnet-ls-lib/src/completion/keyword.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use language_server::{
     cache::Cache,
     completion::{Completion, CompletionResult},
@@ -16,6 +18,41 @@ pub struct KeywordCompletion<'a> {
 impl<'a> KeywordCompletion<'a> {
     pub fn new(cache: &'a Cache<JsonnetASTGenerator>) -> Self {
         Self { cache }
+    }
+}
+
+type ShowFunc = fn(stack: &NodeStack) -> bool;
+
+struct JsonnetKeyword {
+    /// Name of the Keyword
+    name: String,
+    /// Function to call to check if the keyword is valid and should be shown. If None always show
+    /// the keyword
+    show_func: Option<ShowFunc>,
+}
+
+impl JsonnetKeyword {
+    fn new(name: &str, show_func: ShowFunc) -> Self {
+        Self {
+            name: name.into(),
+            show_func: Some(show_func),
+        }
+    }
+
+    fn should_show(&self, stack: &NodeStack) -> bool {
+        match self.show_func {
+            Some(func) => func(stack),
+            None => true,
+        }
+    }
+}
+
+impl<T: Display> From<T> for JsonnetKeyword {
+    fn from(value: T) -> Self {
+        Self {
+            name: value.to_string(),
+            show_func: None,
+        }
     }
 }
 
@@ -49,29 +86,47 @@ fn show_super(stack: &NodeStack) -> bool {
     }
 }
 
+fn show_self(stack: &NodeStack) -> bool {
+    stack
+        .stack
+        .iter()
+        .any(|node| matches!(*node.node_kind, NodeKind::DesugaredObject(_)))
+}
+
 impl<'a> Completion for KeywordCompletion<'a> {
     fn complete(&self, location: Position, uri: &Uri) -> CompletionResult {
         let doc = self.cache.get_document(uri).unwrap();
 
         let stack = doc.get_ast()?.get_stack_by_position(&location.into());
 
-        let show_self = stack
-            .stack
-            .iter()
-            .any(|node| matches!(*node.node_kind, NodeKind::DesugaredObject(_)));
         // TODO: check if keywords are really usable
-        let mut keywords = vec!["local", "import", "importstr"];
-        if show_self {
-            keywords.push("self");
-        }
-        if show_super(&stack) {
-            keywords.push("super");
-        }
+        // Valid keywords: else error false for function if import importstr importbin in local null tailstrict then self super true
+        let keywords: Vec<JsonnetKeyword> = vec![
+            "assert".into(),
+            "else".into(),
+            "error".into(),
+            "false".into(),
+            "for".into(),
+            "function".into(),
+            "if".into(),
+            "import".into(),
+            "importstr".into(),
+            "importbin".into(),
+            "in".into(),
+            "local".into(),
+            "null".into(),
+            "tailstrict".into(),
+            "then".into(),
+            JsonnetKeyword::new("self", show_self),
+            JsonnetKeyword::new("super", show_super),
+            "true".into(),
+        ];
 
         let items = keywords
             .iter()
+            .filter(|keyword| keyword.should_show(&stack))
             .map(|keyword| CompletionItem {
-                label: keyword.to_string(),
+                label: keyword.name.clone(),
                 kind: Some(CompletionItemKind::KEYWORD),
                 ..Default::default()
             })
