756f1e8e8f34d2cb52341937a0e3589b45d5ca02
Kevin <kevin@kokev.de>
2025-09-14 13:55:00 +0200

    Only complete super keyword if it is actually valid

--- crates/grustonnet-ls-lib/src/completion/keyword.rs
+++ crates/grustonnet-ls-lib/src/completion/keyword.rs
@@ -4,7 +4,10 @@ use language_server::{
 };
 use lsp_types::{CompletionItem, CompletionItemKind, CompletionList, Position, Uri};
 
-use crate::{cache::JsonnetASTGenerator, node::types::node_kind::NodeKind};
+use crate::{
+    cache::JsonnetASTGenerator,
+    node::{stack::NodeStack, types::node_kind::NodeKind},
+};
 
 pub struct KeywordCompletion<'a> {
     cache: &'a Cache<JsonnetASTGenerator>,
@@ -13,6 +16,36 @@ pub struct KeywordCompletion<'a> {
 impl<'a> KeywordCompletion<'a> {
     pub fn new(cache: &'a Cache<JsonnetASTGenerator>) -> Self {
         Self { cache }
+    }
+}
+
+fn show_super(stack: &NodeStack) -> bool {
+    // Find the first parent binary
+    let first_binary = stack.stack.iter().rev().find_map(|node| {
+        if let NodeKind::Binary(bin) = node.node_kind.as_ref() {
+            Some(bin)
+        } else {
+            None
+        }
+    });
+    // If the left one has an object -> super is valid
+    if let Some(first_binary) = first_binary {
+        match first_binary.left.node_kind.as_ref() {
+            NodeKind::DesugaredObject(_) => {
+                // If the left one is the same as the current object we only have rights in the
+                // binary
+                first_binary.left != stack.peek().unwrap_or_default()
+            }
+            NodeKind::Binary(bin) => {
+                let nodes = bin.flatten();
+                nodes
+                    .iter()
+                    .any(|n| matches!(n.node_kind.as_ref(), NodeKind::DesugaredObject(_)))
+            }
+            _ => false,
+        }
+    } else {
+        false
     }
 }
 
@@ -27,9 +60,12 @@ impl<'a> Completion for KeywordCompletion<'a> {
             .iter()
             .any(|node| matches!(*node.node_kind, NodeKind::DesugaredObject(_)));
         // TODO: check if keywords are really usable
-        let mut keywords = vec!["local", "import", "importstr", "super"];
+        let mut keywords = vec!["local", "import", "importstr"];
         if show_self {
             keywords.push("self");
+        }
+        if show_super(&stack) {
+            keywords.push("super");
         }
 
         let items = keywords
