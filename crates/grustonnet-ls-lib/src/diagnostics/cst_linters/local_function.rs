b515a47df8fafcfc7c4da864658d71001f62241d
Kevin <kevin@kokev.de>
2025-11-09 15:45:57 +0100

    feat(diag): Update nolint query to only disable the lint for the next line

--- crates/grustonnet-ls-lib/src/diagnostics/cst_linters/local_function.rs
+++ crates/grustonnet-ls-lib/src/diagnostics/cst_linters/local_function.rs
@@ -31,27 +31,35 @@ impl JsonnetDiagnosticFilter {
     fn should_ignore(&self, uri: &Uri, loc_range: LocationRange) -> Option<bool> {
         let doc = self.cache.get_document(uri).ok()?;
         let tree = new_tree(&doc.content)?;
-        let query_source = r#"((comment) @comment (#eq? @comment "// nolint"))"#;
+        let query_source = r#"
+            (
+                (comment) @comment (#eq? @comment "// nolint")
+                (local_bind (local) (bind) @local_sibling)?
+                ((_) @generic_sibling)?
+            )
+        "#;
         let query = Query::new(&tree.language(), query_source).expect("BUG: Invalid query");
 
         let mut cursor = QueryCursor::new();
         let mut captures = cursor.matches(&query, tree.root_node(), doc.content.as_bytes());
 
         Some(captures.any(|cap| {
-            cap.captures.iter().any(|c| match c.node.next_sibling() {
-                None => false,
-                Some(sibling) => {
-                    let sibling_begin: Location = sibling.start_position().into();
-                    let sibling_end: Location = sibling.end_position().into();
-                    let sibling_range = LocationRange {
-                        begin: sibling_begin,
-                        end: sibling_end,
-                        ..Default::default()
-                    };
-                    let in_range = sibling_range.in_range(&loc_range.begin);
-                    in_range
-                }
-            })
+            let Some(sibling) = cap.captures.iter().find(|c| {
+                c.index
+                    == query
+                        .capture_index_for_name("local_sibling")
+                        .unwrap_or(query.capture_index_for_name("generic_sibling").unwrap())
+            }) else {
+                return false;
+            };
+            let sibling_begin: Location = sibling.node.start_position().into();
+            let sibling_end: Location = sibling.node.end_position().into();
+            let sibling_range = LocationRange {
+                begin: sibling_begin,
+                end: sibling_end,
+                ..Default::default()
+            };
+            sibling_range.in_range(&loc_range.begin)
         }))
     }
 }
