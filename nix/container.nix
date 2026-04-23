53483af87f9c37f39b0d0f141d08967c154eb714
Kevin <kevin@kokev.de>
2026-04-23 17:07:09 +0200

    chore(container): Add bash and coreutils to container to make it usable with gitlab ci

--- nix/container.nix
+++ nix/container.nix
@@ -12,24 +12,31 @@ _: {
     {
       packages =
         let
-          binaries =
+          mkBinaries =
+            name:
             pkgs.runCommand "bins"
               {
                 nativeBuildInputs = [ pkgs.removeReferencesTo ];
               }
               ''
-                          mkdir -p $out/bin
-                          cp ${self'.packages.grustonnet}/bin/grustonnet-* $out/bin/
+                mkdir -p $out/bin
+                cp ${self'.packages.grustonnet}/bin/grustonnet-${name} $out/bin/
                 # Remove all go references to avoid bloating the image
-                          remove-references-to -t ${pkgs.go} $out/bin/*
+                remove-references-to -t ${pkgs.go} $out/bin/*
               '';
           createContainer =
             name:
+            let
+              binaries = mkBinaries name;
+            in
             nix2containerPkgs.nix2container.buildImage {
               name = "grustonnet-${name}";
               tag = "latest";
               config = {
                 Cmd = [ "${binaries}/bin/grustonnet-${name}" ];
+                Env = [
+                  "PATH=${binaries}/bin:${pkgs.bash}/bin:${pkgs.coreutils}/bin"
+                ];
               };
             };
         in
@@ -37,23 +44,21 @@ _: {
           dockerLinter = createContainer "lint";
           dockerDebugger = createContainer "debugger";
           dockerLs = createContainer "ls";
-          dockerImageFull = nix2containerPkgs.nix2container.buildImage {
-            name = "grustonnet";
-            tag = "latest";
+          dockerImageFull =
+            let
+              binaries = mkBinaries "*";
+            in
+            nix2containerPkgs.nix2container.buildImage {
+              name = "grustonnet";
+              tag = "latest";
 
-            copyToRoot = pkgs.buildEnv {
-              name = "root";
-              paths = [ binaries ];
-              pathsToLink = [ "/bin" ];
+              config = {
+                Cmd = [ "${binaries}/bin/grustonnet-lint" ];
+                Env = [
+                  "PATH=${binaries}/bin:${pkgs.bash}/bin:${pkgs.coreutils}/bin"
+                ];
+              };
             };
-
-            config = {
-              Cmd = [ "${binaries}/bin/grustonnet-lint" ];
-              Env = [
-                "PATH=${binaries}/bin"
-              ];
-            };
-          };
 
         };
     };
