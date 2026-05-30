029a980c0b5ac8cedc4309de01f69385a0a58e65
Kevin <kevin@kokev.de>
2026-05-30 13:16:24 +0200

    chore(nix): Switch to stable nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
     naersk = {
       url = "github:nix-community/naersk/pull/391/head";
