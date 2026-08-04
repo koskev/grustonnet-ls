123a6b6b32b4a7512a307a039b5e3fab2fbe8a22
Kevin <kevin@kokev.de>
2026-08-04 17:16:33 +0200

    chore(build): Allow to pass an environment variable containing the git short hash

--- crates/version-macros/src/lib.rs
+++ crates/version-macros/src/lib.rs
@@ -9,14 +9,18 @@ use proc_macro::TokenStream;
 
 #[proc_macro]
 pub fn git_commit(_input: TokenStream) -> TokenStream {
-    let output = Command::new("git")
-        .args(["rev-parse", "--short", "HEAD"])
-        .output();
-
-    let git_hash = match output {
-        Ok(out) if out.status.success() => String::from_utf8_lossy(&out.stdout).trim().to_string(),
-        _ => "unknown".to_string(),
-    };
+    let git_hash = std::option_env!("GIT_COMMIT_SHORT").map_or_else(
+        || match Command::new("git")
+            .args(["rev-parse", "--short", "HEAD"])
+            .output()
+        {
+            Ok(out) if out.status.success() => {
+                String::from_utf8_lossy(&out.stdout).trim().to_string()
+            }
+            _ => "unknown".to_string(),
+        },
+        |h| h.to_string(),
+    );
 
     format!("\"{}\"", git_hash)
         .parse()
